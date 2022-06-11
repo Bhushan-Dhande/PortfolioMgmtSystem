579b03ec1271f0f86237d4d94e25b3e47c579e6e
bhushan-dhande <bhush.dhande@gmail.com>
2022-06-11 13:29:46 +0200

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PortfolioMgmtSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
